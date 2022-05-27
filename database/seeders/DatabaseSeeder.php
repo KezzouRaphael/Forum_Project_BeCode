a395ab59c4becb99348c2bff40073a73e5099173
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-05-27 11:09:54 +0200

    Seeder completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Topics;
+use App\Models\Boards;
+use App\Models\Posts;
+use App\Models\Forums;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +19,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => "Raphael Kezzou",
+            'email' => "raphael.kezzou@live.fr"
+        ]);
+        Boards::factory(4)->create([
+            'create_id' => $user->id,
+            'update_id' => $user->id
+        ]);
+        $forum = Forums::factory()->create([
+            'create_id' => $user->id,
+            'update_id' => $user->id
+        ]);
+        Topics::factory(10)->create([
+            'forum' => $forum->id,
+            'create_id' => $user->id,
+            'update_id' => $user->id
+        ]);
+        Posts::factory(10)->create([
+            'post' => $user->id
+        ]);
+        $user = User::factory()->create([
+            'name' => "user",
+            'email' => "user@user.com"
+        ]);
     }
 }
