735fbd1a3e09281d0e9313dd42173fdfc07e566e
AbielKiflu <abiel.kiflu.weld@gmail.com>
2022-06-02 14:12:54 +0200

    BoardController created

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+
+class BoardController extends Controller
+{
+    
+    // create board 
+    public function create(Request $request){
+
+    }
+
+
+
+    // edit board
+
+
+
+
+
+    // delete board
+
+
+    // show board
+
+
+
+
+    //show boards
+
+}
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,31 +1,38 @@
 <?php
+ namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
-
-use App\Models\User;
-use Illuminate\Validation\Rule;
-use Illuminate\Http\Request;
-
-class RegisterController extends Controller
-{
-    public function create()
-    {
-        return view('register.create');
-    }
-
-    public function store()
-    {
-        $attributes = request()->validate([
-            'nickname' => ['required', 'max:255', Rule::unique('users', 'nickname')],
-            'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
-            'password' => ['required', 'max:255'],
-            'signature' => ['required', 'max:255']
-        ]);
-
-        $user = User::create($attributes);
-
-        Auth()->login($user);
-        //return redirect()->route('login', []);
-        return redirect('/')->with('success', 'Your account has been created.');
-    }
-}
+ use App\Models\User;
+ use Illuminate\Validation\Rule;
+ use Illuminate\Http\Request;
+ use App\Http\Controllers\AuthUserController;
+ use Illuminate\Support\Facades\Hash;
+ 
+ class RegisterController extends Controller
+ {
+     public function create()
+     {
+         return view('register.create');
+     }
+ 
+     public function store()
+     {
+         
+         $attributes = request()->validate([
+             'nickname' => ['required', 'max:255', Rule::unique('users', 'nickname')],
+             'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
+             'password' => ['required', 'max:255'],
+             'signature' => ['required', 'max:255']
+         ]);
+ 
+         $user = new User();
+         $user->nickname = $attributes['nickname'];
+         $user->email=$attributes['email'];
+         $user->signature= $attributes['signature'];
+         $user->password=Hash::make($attributes['password']);
+         $user->save();
+ 
+ 
+         return redirect()->action([AuthUserController::class,'login'],['email'=> $user->email,'password'=>$attributes['password']]);
+         
+     }
+ }
