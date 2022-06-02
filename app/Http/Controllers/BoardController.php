fa2657fdd7b13a3a4756fa6ae978ae172d5de16b
AbielKiflu <abiel.kiflu.weld@gmail.com>
2022-06-02 14:35:47 +0200

    Create board is done

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Boards;
 
 
 
 class BoardController extends Controller
 {
     
+    
+
     // create board 
     public function create(Request $request){
 
+
+        $attributes = request()->validate([
+            'description' => ['required']
+        ]);
+        $board=new Boards();
+        $board->description = $attributes['description'];
+        $board->create_id = Auth::id();;
+        $board->update_id = Auth::id();;
+        $board->created_at = now();
+        $board->updated_at = now();
+        $board->save();
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthUserController;
 use App\Http\Controllers\RegisterController;
-
+use App\Http\Controllers\BoardController;
 
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {return $request->user();});
@@ -18,5 +18,6 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthUserController::class, 'user']);
+    Route::post('board/create', [BoardController::class, 'create']);
     Route::get('logout', [AuthUserController::class, 'logout']);
 });
