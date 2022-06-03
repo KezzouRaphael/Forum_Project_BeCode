6d89bc389a75ec7ad896999b8a968d7193930f72
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-06-03 15:46:17 +0200

    Routes fix

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\PostController;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {return $request->user();});
 
 
- 
+
 
 
 //user
@@ -22,13 +22,7 @@ Route::get('login', [AuthUserController::class, 'login']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-//board
-Route::get('board/boards', [BoardController::class, 'boards']);
-Route::get('board/board', [BoardController::class, 'board']);
 
-//post
-Route::get('post/boards', [PostController::class, 'boards']);
-Route::get('post/board', [PostController::class, 'board']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -37,25 +31,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthUserController::class, 'user']);
     Route::get('logout', [AuthUserController::class, 'logout']);
 
- 
+    //forums
     Route::get('forums/create', [ForumController::class, 'create']);
     Route::post('forums/store', [ForumController::class, 'store']);
     Route::put('forums/edit', [ForumController::class, 'edit']);
     Route::delete('forums/delete', [ForumController::class, 'delete']);
 
+    //topic
     Route::get('topics/create', [TopicController::class, 'create']);
     Route::post('topics/store', [TopicController::class, 'store']);
     Route::put('topics/edit', [TopicController::class, 'edit']);
     Route::delete('topics/delete', [TopicController::class, 'delete']);
-
-    Route::post('board/create', [BoardController::class, 'create']);
-    Route::put('board/edit', [BoardController::class, 'edit']);
-    Route::delete('board/delete', [BoardController::class, 'delete']);
-});
-
-Route::get('board/board', [BoardController::class, 'board']);
-Route::get('forums/forum', [ForumController::class, 'forum']);
-Route::get('topics/topic', [TopicController::class, 'topic']);
 
     //board
     Route::post('board/create', [BoardController::class, 'create']);
@@ -66,8 +52,15 @@ Route::get('topics/topic', [TopicController::class, 'topic']);
     Route::post('post/create', [PostController::class, 'create']);
     Route::put('post/edit', [PostController::class, 'edit']);
     Route::delete('post/delete', [PostController::class, 'delete']);
-
 });
+//board
+Route::get('board/boards', [BoardController::class, 'boards']);
+Route::get('board/board', [BoardController::class, 'board']);
 
+//post
+Route::get('post/boards', [PostController::class, 'boards']);
+Route::get('post/board', [PostController::class, 'board']);
 
-
+Route::get('board/board', [BoardController::class, 'board']);
+Route::get('forums/forum', [ForumController::class, 'forum']);
+Route::get('topics/topic', [TopicController::class, 'topic']);
