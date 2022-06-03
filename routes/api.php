55b72c233d7701d45469a7850c6199d9db1a9977
AbielKiflu <abiel.kiflu.weld@gmail.com>
2022-06-03 13:13:48 +0200

    post controller created with crud, not tested

--- routes/api.php
+++ routes/api.php
@@ -11,19 +11,28 @@ use App\Http\Controllers\BoardController;
 
 
 
-Route::get('login', [AuthUserController::class, 'login']);
 
+
+//user
+Route::get('login', [AuthUserController::class, 'login']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+//board
 Route::get('board/boards', [BoardController::class, 'boards']);
+Route::get('board/board', [BoardController::class, 'board']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    //user
     Route::get('user', [AuthUserController::class, 'user']);
+    Route::get('logout', [AuthUserController::class, 'logout']);
+    
+    //board
     Route::post('board/create', [BoardController::class, 'create']);
     Route::put('board/edit', [BoardController::class, 'edit']);
     Route::delete('board/delete', [BoardController::class, 'delete']);
-    Route::get('board/board', [BoardController::class, 'board']);
-    Route::get('logout', [AuthUserController::class, 'logout']);
+
 });
 
 
