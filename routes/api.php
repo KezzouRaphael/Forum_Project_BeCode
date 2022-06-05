2878a2be4896617762badb5afcefe9238ad34e50
AbielKiflu <abiel.kiflu.weld@gmail.com>
2022-06-05 18:40:21 +0200

    id parametes added to each routes

--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,19 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 
 //board
 Route::get('board/boards', [BoardController::class, 'boards']);
-Route::get('board/board', [BoardController::class, 'board']);
+Route::get('board/board/{id}', [BoardController::class, 'board']);
 
  
 // forum
-Route::get('forums/forum', [ForumController::class, 'forum']);
+Route::get('forums/forum/{id}', [ForumController::class, 'forum']);
 Route::get('forums/forums', [ForumController::class, 'forums']);
 
 // topic
-Route::get('topics/topic', [TopicController::class, 'topic']);
+Route::get('topics/topic/{id}', [TopicController::class, 'topic']);
 Route::get('topics/topics', [TopicController::class, 'topics']);
 
 //post
-Route::post('post/post', [PostController::class, 'create']);
+Route::post('post/post/{id}', [PostController::class, 'create']);
 Route::put('post/posts', [PostController::class, 'edit']);
 
 
@@ -51,24 +51,23 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // forum
     Route::get('forums/create', [ForumController::class, 'create']);
-    Route::put('forums/edit', [ForumController::class, 'edit']);
-    Route::delete('forums/delete', [ForumController::class, 'delete']);
+    Route::put('forums/edit/{id}', [ForumController::class, 'edit']);
+    Route::delete('forums/delete/{id}', [ForumController::class, 'delete']);
     
     // topic
     Route::get('topics/create', [TopicController::class, 'create']);
-    Route::post('topics/store', [TopicController::class, 'store']);
-    Route::put('topics/edit', [TopicController::class, 'edit']);
-    Route::delete('topics/delete', [TopicController::class, 'delete']);
+    Route::put('topics/edit/{id}', [TopicController::class, 'edit']);
+    Route::delete('topics/delete/{id}', [TopicController::class, 'delete']);
 
     // board
     Route::post('board/create', [BoardController::class, 'create']);
-    Route::put('board/edit', [BoardController::class, 'edit']);
-    Route::delete('board/delete', [BoardController::class, 'delete']);
+    Route::put('board/edit/{id}', [BoardController::class, 'edit']);
+    Route::delete('board/delete/{id}', [BoardController::class, 'delete']);
 
     //post
     Route::post('post/create', [PostController::class, 'create']);
-    Route::put('post/edit', [PostController::class, 'edit']);
-    Route::delete('post/delete', [PostController::class, 'delete']);
+    Route::put('post/edit/{id}', [PostController::class, 'edit']);
+    Route::delete('post/delete/{id}', [PostController::class, 'delete']);
 
 
 });
