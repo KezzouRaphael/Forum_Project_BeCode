34a3683c44a88c09ea721a0bfb47c1986dfcca37
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-06-15 09:56:54 +0200

    Logout fix

--- app/Http/Controllers/AuthUserController.php
+++ app/Http/Controllers/AuthUserController.php
@@ -38,9 +38,10 @@ class AuthUserController extends Controller
     }
 
 
-    public function logout()
+    public function logout(Request $request)
     {
         $cookie = Cookie::forget("jwt");
+        $request->session()->invalidate();
         return response([
             "message" => "success"
         ], 200)->withCookie($cookie);
